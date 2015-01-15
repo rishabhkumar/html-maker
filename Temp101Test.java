a3afd8a228da4fedf6ba2479608a6d202fe8fee1
Devang Gaur <dg7.testing@gmail.com>
2015-01-15 18:11:53 +0530

    Cleansed and Standardised the code.
    Improved the Coding coventions which made the code able for other
    developers to understand better and quick.

---
+++ Temp101Test.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+public class Temp101Test{
+ public static void main(String... s)
+    {
+	Temp101 temp = new Temp101("GUI - 15");
+	temp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	temp.setSize(500, 500);
+	temp.setVisible(true);
+    }
+}
